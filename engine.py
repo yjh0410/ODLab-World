dc531220ea0266ae8d49c6f1d3a6ced214452920
yjh0410 <1394571815@qq.com>
2024-02-21 17:33:43 +0800

    update

--- engine.py
+++ engine.py
@@ -280,9 +280,14 @@ class YoloTrainer(object):
         """
         # During training phase, the shape of input image is square.
         old_img_size = images.shape[-1]
-        new_img_size = random.randrange(old_img_size * multi_scale_range[0], old_img_size * multi_scale_range[1] + max_stride)
-        new_img_size = new_img_size // max_stride * max_stride  # size
-        if new_img_size / old_img_size != 1:
+        min_img_size = old_img_size * multi_scale_range[0]
+        max_img_size = old_img_size * multi_scale_range[1]
+
+        # Choose a new image size
+        new_img_size = random.randrange(min_img_size, max_img_size + max_stride, max_stride)
+        
+        # Resize
+        if new_img_size != old_img_size:
             # interpolate
             images = torch.nn.functional.interpolate(
                                 input=images, 
@@ -290,20 +295,21 @@ class YoloTrainer(object):
                                 mode='bilinear', 
                                 align_corners=False)
         # rescale targets
-        for tgt in targets:
-            boxes = tgt["boxes"].clone()
-            labels = tgt["labels"].clone()
-            boxes = torch.clamp(boxes, 0, old_img_size)
-            # rescale box
-            boxes[:, [0, 2]] = boxes[:, [0, 2]] / old_img_size * new_img_size
-            boxes[:, [1, 3]] = boxes[:, [1, 3]] / old_img_size * new_img_size
-            # refine tgt
-            tgt_boxes_wh = boxes[..., 2:] - boxes[..., :2]
-            min_tgt_size = torch.min(tgt_boxes_wh, dim=-1)[0]
-            keep = (min_tgt_size >= 1)
+        if not self.cfg.normalize_coords:
+            for tgt in targets:
+                boxes = tgt["boxes"].clone()
+                labels = tgt["labels"].clone()
+                boxes = torch.clamp(boxes, 0, old_img_size)
+                # rescale box
+                boxes[:, [0, 2]] = boxes[:, [0, 2]] / old_img_size * new_img_size
+                boxes[:, [1, 3]] = boxes[:, [1, 3]] / old_img_size * new_img_size
+                # refine tgt
+                tgt_boxes_wh = boxes[..., 2:] - boxes[..., :2]
+                min_tgt_size = torch.min(tgt_boxes_wh, dim=-1)[0]
+                keep = (min_tgt_size >= 1)
 
-            tgt["boxes"] = boxes[keep]
-            tgt["labels"] = labels[keep]
+                tgt["boxes"] = boxes[keep]
+                tgt["labels"] = labels[keep]
 
         return images, targets, new_img_size
 
@@ -577,9 +583,14 @@ class RTDetrTrainer(object):
         """
         # During training phase, the shape of input image is square.
         old_img_size = images.shape[-1]
-        new_img_size = random.randrange(old_img_size * multi_scale_range[0], old_img_size * multi_scale_range[1] + max_stride)
-        new_img_size = new_img_size // max_stride * max_stride  # size
-        if new_img_size / old_img_size != 1:
+        min_img_size = old_img_size * multi_scale_range[0]
+        max_img_size = old_img_size * multi_scale_range[1]
+
+        # Choose a new image size
+        new_img_size = random.randrange(min_img_size, max_img_size + max_stride, max_stride)
+        
+        # Resize
+        if new_img_size != old_img_size:
             # interpolate
             images = torch.nn.functional.interpolate(
                                 input=images, 
