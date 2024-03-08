0134edf350a8352477b5a46423dd681651903916
yjh0410 <1394571815@qq.com>
2024-03-08 23:02:01 +0800

    modify lr scheduler

--- engine.py
+++ engine.py
@@ -133,6 +133,7 @@ class YoloTrainer(object):
                 torch.save({'model': model_eval.state_dict(),
                             'mAP': -1.,
                             'optimizer': self.optimizer.state_dict(),
+                            'lr_scheduler': self.lr_scheduler.state_dict(),
                             'epoch': self.epoch,
                             'args': self.args}, 
                             checkpoint_path)               
@@ -154,6 +155,7 @@ class YoloTrainer(object):
                     torch.save({'model': model_eval.state_dict(),
                                 'mAP': round(self.best_map*100, 1),
                                 'optimizer': self.optimizer.state_dict(),
+                                'lr_scheduler': self.lr_scheduler.state_dict(),
                                 'epoch': self.epoch,
                                 'args': self.args}, 
                                 checkpoint_path)                      
