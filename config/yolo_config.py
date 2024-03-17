fc649ea4966af218df65cb05781f10c202d30097
yjh0410 <1394571815@qq.com>
2024-03-17 21:34:44 +0800

    update

--- config/yolo_config.py
+++ config/yolo_config.py
@@ -78,7 +78,7 @@ class YoloBaseConfig(object):
         self.optimizer    = 'adamw'
         self.per_image_lr = 0.001 / 64
         self.base_lr      = None      # base_lr = per_image_lr * batch_size
-        self.min_lr_ratio = 0.05      # min_lr  = base_lr * min_lr_ratio
+        self.min_lr_ratio = 0.01      # min_lr  = base_lr * min_lr_ratio
         self.momentum     = 0.9
         self.weight_decay = 0.05
         self.clip_max_norm   = -1.
